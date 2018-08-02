b0bb5905dc7b71e8adce60d6f62c0207fbdc4ab2
Igor karlo <igor.karanovic@live.com>
2018-08-02 14:20:22 +0200

    - htaccess changes & link added in list

--- view/index.php
+++ view/index.php
@@ -1,5 +1,10 @@
 <?php
 require_once '../app/db.php';
+$url = 'http://svismomiizistogfilma.com';
+
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+  $url = 'http://localhost/sff2018';
+}
 
 $sql = "
 SELECT 
@@ -7,38 +12,50 @@ u.id as user_id,
 u.name,
 u.lastname,
 u.email,
+u.country,
+u.ip,
 f.name as filename,
-f.filesize as 'filesize (kb)'
+f.filesize as 'filesize (kb)',
+f.extension
 FROM users as u
 LEFT JOIN files as f ON u.id = f.user_id
 WHERE u.id > 41 order by u.id desc;";
 
 $res = mysqli_query($connection, $sql);
-
-echo '<table><thead>
-<tr>
-    <td>user id</td>
-    <td>name</td>
-    <td>email</td>
-    <td>filename</td>
-    <td>size</td>
-</tr>
-</thead> <tbody>';
-
-//$res = $res->fetch_array();
-//var_dump();
-$res->fetch_assoc();
-foreach ($res as $item) {
-
-  echo "<tr>
-    <td>{$item['user_id']}</td>
-    <td>{$item['name']} {$item['lastname']}</td>
-    <td>{$item['email']}</td>
-    <td>{$item['filename']}</td>
-    <td>{$item['filesize (kb)']}</td>
-</tr>";
-}
-echo '</tbody></table>';
-
-
-//var_dump($res->fetch_assoc());
+?>
+<table>
+  <thead>
+    <tr>
+        <td>user id</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Country</td>
+        <td>File Name</td>
+        <td>File size</td>
+        <td>File type</td>
+        <td>IP</td>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+  $res->fetch_assoc();
+  foreach ($res as $item):
+  $link = $url . '/uploads/' . $item['user_id'] . '/' . $item['filename'];
+  ?>
+  <tr>
+    <td><?php echo $item['user_id']; ?></td>
+    <td><?php echo $item['name']; ?> <?php echo $item['lastname']; ?></td>
+    <td><?php echo $item['email']; ?></td>
+    <td><?php echo $item['country']; ?></td>
+    <td>
+      <a href="<?php echo $link; ?>" target="_blank">
+        <?php echo $item['filename']; ?>
+      </a>
+    </td>
+    <td><?php echo $item['filesize (kb)']; ?></td>
+    <td><?php echo $item['extension']; ?></td>
+    <td><?php echo $item['ip']; ?></td>
+  </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
